50fb16cf3d044e3e5a501c6a22c40267904759d0
Leah Johnson <leahbowie@outlook.com>
2024-05-10 17:55:30 -0600

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,35 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "First Game" };
-    window.setFramerateLimit(144);
+int main() {
+    auto window = sf::RenderWindow{ { 680u, 480u }, "First Game" };
+    window.setFramerateLimit(60);
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
+    // create an array of 3 vertices that define a triangle primitive
+    sf::VertexArray triangle(sf::Triangles, 3);
+
+    // define the position of the triangle's points
+    triangle[0].position = sf::Vector2f(10.f, 10.f);
+    triangle[1].position = sf::Vector2f(100.f, 10.f);
+    triangle[2].position = sf::Vector2f(100.f, 100.f);
+
+    // define the color of the triangle's points
+    triangle[0].color = sf::Color::Red;
+    triangle[1].color = sf::Color::Blue;
+    triangle[2].color = sf::Color::Green;
+
+    // no texture coordinates here, we'll see that later
+
+    while (window.isOpen()) {
+        for (auto event = sf::Event{}; window.pollEvent(event);) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
 
         window.clear();
+
+        window.draw(triangle);
+
         window.display();
     }
 }
