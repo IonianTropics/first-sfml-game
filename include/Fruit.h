1f533c01f04ac522a3a2d981ce5c1606d9a70b86
Leah Johnson <leahbowie@outlook.com>
2024-05-14 14:59:33 -0600

    feat(fruit): add fruit

---
+++ include/Fruit.h
@@ -0,0 +1,10 @@
+#ifndef GAME_FRUIT_H_
+#define GAME_FRUIT_H_
+
+namespace game {
+class Fruit {
+
+};
+} // namespace game
+
+#endif // GAME_FRUIT_H_
---
+++ src/Fruit.cpp
@@ -0,0 +1,5 @@
+#include "Fruit.h"
+
+namespace game {
+    
+} // namespace game
