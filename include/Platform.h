8a9326ba30e0b24dc56eb3f1fe2cca7a1e96c29c
Leah Johnson <leahbowie@outlook.com>
2024-05-14 14:59:45 -0600

    feat(platform): add platform

---
+++ include/Platform.h
@@ -0,0 +1,10 @@
+#ifndef GAME_PLATFORM_H_
+#define GAME_PLATFORM_H_
+
+namespace game {
+class Platform {
+
+};
+} // namespace game
+
+#endif // GAME_PLATFORM_H_
---
+++ src/Platform.cpp
@@ -0,0 +1,5 @@
+#include "Platform.h"
+
+namespace game {
+    
+} // namespace game
