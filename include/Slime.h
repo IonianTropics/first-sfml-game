391bb79a7d3b8a7637b3697444001fcf34b2daab
Leah Johnson <leahbowie@outlook.com>
2024-05-14 14:59:59 -0600

    feat(slime): add slime

---
+++ include/Slime.h
@@ -0,0 +1,10 @@
+#ifndef GAME_SLIME_H_
+#define GAME_SLIME_H_
+
+namespace game {
+class Slime {
+
+};
+} // namespace game
+
+#endif // GAME_SLIME_H_
---
+++ src/Slime.cpp
@@ -0,0 +1,5 @@
+#include "Slime.h"
+
+namespace game {
+    
+} // namespace game
